bd28bcc23c8714a53c30fc4a8377e58d91e4c83e
Adityaa-Sharma <mailmeifyoucan7@gmail.com>
2025-02-25 01:40:19 +0530

    adding the Gelu and FFNN class

--- Linformer.py
+++ Linformer.py
@@ -77,7 +77,28 @@ class MultiHeadLinearAttention(nn.Module):
         out=torch.cat([head(x) for head in self.heads],dim=-1)#concatenating along the head size --> (B,T,C)
         out=self.proj(out)
         return out
-    
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * 
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
+
+class FeedForward(nn.Module):
+    def __init__(self,n_embed):
+        super().__init__()
+        self.net=nn.Sequential(
+            nn.Linear(n_embed,4*n_embed),
+            nn.GELU(),
+            nn.Linear(4*n_embed,n_embed)
+        )
+            
+    def forward(self,x):
+        return self.net(x)
         
         
         
