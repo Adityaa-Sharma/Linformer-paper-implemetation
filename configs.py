835bc40f1c0f9e2841a1d1d87f09bf17f60463ad
Adityaa-Sharma <mailmeifyoucan7@gmail.com>
2025-03-02 21:54:50 +0530

    adding the readme.md

--- configs.py
+++ configs.py
@@ -10,13 +10,13 @@ class ModelConfig:
     max_iters: int = 240000
     eval_interval: int = 5000
     eval_iter: int = 50
-    learning_rate: float = 1e-5 # gpt paper
+    learning_rate: float = 1e-5
     n_epochs: int = 1
     
     # Model architecture
     n_embed: int = 384
-    n_layer: int = 12 
-    n_head: int = 12 
+    n_layer: int = 6
+    n_head: int = 6
     dropout: float = 0.1
     
     # Linformer parameters
